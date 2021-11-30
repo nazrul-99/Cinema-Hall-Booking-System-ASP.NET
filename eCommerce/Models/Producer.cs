f300f0677bce78b15cec261eb29930b06546717d
Md Nazrul Islam <md.nazrul.islam.7.1999@gmail.com>
2021-11-30 14:43:15 +0600

    producer FullName has been fixed

--- eCommerce/Models/Producer.cs
+++ eCommerce/Models/Producer.cs
@@ -11,7 +11,7 @@ namespace eCommerce.Models
         [Key]
         public int ID { get; set; }
         public string ProfilePictureURL { get; set; }
-        public string Fullname { get; set; }
+        public string FullName { get; set; }
         public string Bio { get; set; }
 
 
