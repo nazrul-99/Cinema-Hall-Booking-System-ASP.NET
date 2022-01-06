2e12a7476c72054c80828c0d0a4e8c46505dd844
Md Nazrul Islam <md.nazrul.islam.7.1999@gmail.com>
2022-01-06 13:13:37 +0600

    Adding actor service interface inorder to add the actor service

---
+++ eCommerce/Data/Services/IActorService.cs
@@ -0,0 +1,18 @@
+﻿using eCommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Data.Services
+{
+    public interface IActorService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+
+    }
+}
