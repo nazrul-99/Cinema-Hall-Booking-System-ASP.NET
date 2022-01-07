263fb0457e1b28f60e33bf3ea2353d0fb132457d
Md Nazrul Islam <md.nazrul.islam.7.1999@gmail.com>
2022-01-07 14:57:23 +0600

    Producer edit and delete functionality have been added.

--- eCommerce/Controllers/ProducerController.cs
+++ eCommerce/Controllers/ProducerController.cs
@@ -1,5 +1,6 @@
 ﻿using eCommerce.Data;
 using eCommerce.Data.Services;
+using eCommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -30,5 +31,60 @@ namespace eCommerce.Controllers
             if (producerDetails == null) return View("NotFound");
             return View(producerDetails);
         }
+
+        //GET: Producer/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ProfilePictureURL,FullName,Bio")] Producer producer)
+        {
+            if (!ModelState.IsValid) return View(producer);
+
+            await _service.AddAsync(producer);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //GET: Producers/Edit/ID
+        public async Task<IActionResult> Edit(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,ProfilePictureURL,FullName,Bio")] Producer producer)
+        {
+            if (!ModelState.IsValid) return View(producer);
+
+            if (id == producer.ID)
+            {
+                await _service.UpdateAsync(id, producer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(producer);
+        }
+
+        //GET: Producers/Delete/ID
+        public async Task<IActionResult> Delete(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+            return View(producerDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
--- eCommerce/Models/Producer.cs
+++ eCommerce/Models/Producer.cs
@@ -12,10 +12,14 @@ namespace eCommerce.Models
         [Key]
         public int ID { get; set; }
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is Required")]
         public string ProfilePictureURL { get; set; }
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is not Valid")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name should be in between 3 to 50 characters")]
         public string FullName { get; set; }
         [Display(Name = "Bio ")]
+        [Required(ErrorMessage = "Bio is not Given")]
         public string Bio { get; set; }
 
 
