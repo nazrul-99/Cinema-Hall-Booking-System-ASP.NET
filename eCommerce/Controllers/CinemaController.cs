88a3d05c62780cc809711a9d3e75522a55c52029
Md Nazrul Islam <md.nazrul.islam.7.1999@gmail.com>
2022-01-07 16:43:01 +0600

    Cinema delete is added.

--- eCommerce/Controllers/CinemaController.cs
+++ eCommerce/Controllers/CinemaController.cs
@@ -62,5 +62,23 @@ namespace eCommerce.Controllers
             await _service.UpdateAsync(id, cinema);
             return RedirectToAction(nameof(Index));
         }
+
+        //Get: Cinemas/Delete/ID
+        public async Task<IActionResult> Delete(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
