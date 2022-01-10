0227db08cf38d4cdaa2b4f08a1dd58bbc3f4036d
Md Nazrul Islam <md.nazrul.islam.7.1999@gmail.com>
2022-01-10 16:37:54 +0600

    Shopping Cart data model has been added. It uses the shopping cart item id and other attribute to calculate the total sum.

---
+++ eCommerce/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public AppDbContext _context { get; set; }
+
+        public string ShoppingCartID { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            // here "??" has been used to check if null
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartID == ShoppingCartID).Include(n => n.Movie).ToList());
+        }
+
+        public double GetShoppingCartTotal() => _context.ShoppingCartItems.Where(n => n.ShoppingCartID == ShoppingCartID).Select(n => n.Movie.Price * n.Amount).Sum();
+    }
+}
