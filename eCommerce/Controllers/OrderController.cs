b2379945097c4cdc8f7d5d32a2f726ae4182fcd5
Md Nazrul Islam <md.nazrul.islam.7.1999@gmail.com>
2022-01-13 15:53:06 +0600

    Order Controller is added containg shopping view model.

---
+++ eCommerce/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using eCommerce.Data.Cart;
+using eCommerce.Data.Services.eTickets.Data.Services;
+using eCommerce.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IMovieService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderController(IMovieService moviesService, ShoppingCart shoppingCart)
+        {
+            _moviesService = moviesService;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+    }
+}
