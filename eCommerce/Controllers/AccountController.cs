7f84573495467a7bbda1ddd2d2ba534ddf29bb0b
Md Nazrul Islam <md.nazrul.islam.7.1999@gmail.com>
2022-01-15 01:04:04 +0600

    Account controller for the user is added. This controller is used for the user who is currently logged into the website.

--- eCommerce/Controllers/AccountController.cs
+++ eCommerce/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +11,16 @@ namespace eCommerce.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
