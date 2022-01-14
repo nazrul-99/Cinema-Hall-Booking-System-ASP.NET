facc899769a16ddbf041e0652c83f75efa096c50
Md Nazrul Islam <md.nazrul.islam.7.1999@gmail.com>
2022-01-14 09:48:52 +0600

    Add to cart button in functional.

--- eCommerce/Controllers/OrderController.cs
+++ eCommerce/Controllers/OrderController.cs
@@ -33,5 +33,16 @@ namespace eCommerce.Controllers
 
             return View(response);
         }
+
+        public async Task<RedirectToActionResult> AddItemToShoppingCart(int iD)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(iD);
+
+            if (item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCartSummary));
+        }
     }
 }
