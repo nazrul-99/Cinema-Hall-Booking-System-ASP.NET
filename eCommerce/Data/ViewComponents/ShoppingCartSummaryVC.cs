ea6ee62f1bc69009e26a6c59aa1a58c907f30684
Md Nazrul Islam <md.nazrul.islam.7.1999@gmail.com>
2022-01-14 11:01:07 +0600

    Shopping Cart Summary view component has been addded to the navigation bar.

---
+++ eCommerce/Data/ViewComponents/ShoppingCartSummaryVC.cs
@@ -0,0 +1,25 @@
+﻿using eCommerce.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Data.ViewComponents
+{
+    public class ShoppingCartSummaryVC: ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummaryVC(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            return View(items.Count);
+        }
+    }
+}
